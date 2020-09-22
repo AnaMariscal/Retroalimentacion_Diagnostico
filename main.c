d38d50d1951279e458685f9792b1434cb882c325
Ana Mariscal <ana.mariscal6554@alumnos.udg.mx>
2020-09-21 20:48:23 -0500

    primer menú de opciones completado

--- main.c
+++ main.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 
 int main(){
-    printf("hola mundo\n");
+    char op;
+
+    do
+    {
+        printf("1. Capturar enteros\n");
+        printf("2. Mostrar cadena n veces\n");
+        printf("3. Agregar personaje\n");
+        printf("0. Salir\n");
+        scanf("%c", &op);
+
+        fflush(stdin);
+    }while(op != '0');
 
 
 
